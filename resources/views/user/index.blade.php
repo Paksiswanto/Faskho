a143f480060cdf210199a22637b5405166558b3c
paksiswanto <unitedkuryu@gmail.com>
2023-02-24 10:21:42 +0700

    n

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,7 +15,7 @@
     <link rel="icon" href="img/core-img/mkmk-removebg-preview.png">
 
     <!-- Core Stylesheet -->
-    <link href="{{asset ('yummy-master/yummy-master/style.css') }}" rel="stylesheet">
+    <link href="{{asset('yummy-master/yummy-master/style.css')}}" rel="stylesheet">
     <style>
 .search-container {
   display: flex;
@@ -114,7 +114,7 @@
         <!-- Single Slide -->
         <div class="welcome-single-slide">
             <!-- Post Thumb -->
-            <img src="{{asset ('yummy-master/yummy-master/img/bg-img/slide-3.jpg') }}" alt="">
+            <img src="{{asset('yummy-master/yummy-master/img/bg-img/slide-3.jpg')}}" alt="">
             <!-- Overlay Text -->
             <div class="project_title">
                 <div class="post-date-commnents d-flex">
@@ -130,7 +130,7 @@
         <!-- Single Slide -->
         <div class="welcome-single-slide">
             <!-- Post Thumb -->
-            <img src="{{asset ('yummy-master/yummy-master/img/bg-img/slide-4.jpg') }}" alt="">
+            <img src="{{asset('yummy-master/yummy-master/img/bg-img/slide-4.jpg')}}" alt="">
             <!-- Overlay Text -->
             <div class="project_title">
                 <div class="post-date-commnents d-flex">
@@ -146,7 +146,7 @@
         <!-- Single Slide -->
         <div class="welcome-single-slide">
             <!-- Post Thumb -->
-            <img src="{{asset ('yummy-master/yummy-master/img/bg-img/slide-4.jpg') }}" alt="">
+            <img src="{{asset('yummy-master/yummy-master/img/bg-img/slide-4.jpg')}}" alt="">
             <!-- Overlay Text -->
             <div class="project_title">
                 <div class="post-date-commnents d-flex">
@@ -175,7 +175,7 @@
                             <div class="single-post wow fadeInUp" data-wow-delay=".2s">
                                 <!-- Post Thumb -->
                                 <div class="post-thumb">
-                                    <img src="{{asset ('yummy-master/yummy-master/img/blog-img/1.jpg') }}" alt="">
+                                    <img src="{{asset('yummy-master/yummy-master/img/blog-img/1.jpg')}}" alt="">
                                 </div>
                                 <!-- Post Content -->
                                 <div class="post-content">
@@ -546,5 +546,83 @@
             <img src="img/instagram-img/2.jpg" alt="">
             <!-- Hover -->
         </div>    
-        @include('layout.footer')
+     
+    <!-- ****** Footer Social Icon Area Start ****** -->
+    <div class="social_icon_area clearfix">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="footer-social-area d-flex">
+                        <div class="single-icon">
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i><span>facebook</span></a>
+                        </div>
+                        <div class="single-icon">
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i><span>Twitter</span></a>
+                        </div>
+                        <div class="single-icon">
+                            <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i><span>GOOGLE+</span></a>
+                        </div>
+                        <div class="single-icon">
+                            <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i><span>linkedin</span></a>
+                        </div>
+                        <div class="single-icon">
+                            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i><span>Instagram</span></a>
+                        </div>
+                        <div class="single-icon">
+                            <a href="#"><i class="fa fa-vimeo" aria-hidden="true"></i><span>VIMEO</span></a>
+                        </div>
+                        <div class="single-icon">
+                            <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i><span>YOUTUBE</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ****** Footer Social Icon Area End ****** -->
+
+    <!-- ****** Footer Menu Area Start ****** -->
+    <footer class="footer_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="footer-content">
+                        <!-- Logo Area Start -->
+                        <div class="footer-logo-area text-center">
+                            <a href="index.html" class="yummy-logo">KulinerKu</a>
+                        </div>
+                        <!-- Menu Area Start -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Copywrite Text -->
+                    <div class="copy_right_text text-center">
+                        <p>KELOMPOK NOVA<i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="" target="_blank">Hummasoft</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- ****** Footer Menu Area End ****** -->
+
+    <!-- Jquery-2.2.4 js -->
+    {{-- <script src="{{asset ('yummy-master/yummy-master/js/jquery/jquery-2.2.4.min.js') }}"></script> --}}
+    <script src="{{asset('yummy-master/yummy-master/js/jquery/jquery-2.2.4.min.js')}}"></script>
+    <!-- Popper js -->
+    {{-- <script src="{{asset ('yummy-master/yummy-master/js/bootstrap/popper.min.js') }}"></script> --}}
+    <script src="{{asset('yummy-master/yummy-master/js/bootstrap/popper.min.js')}}"></script>
+    <!-- Bootstrap-4 js -->
+    {{-- <script src="{{asset ('yummy-master/yummy-master/js/bootstrap/bootstrap.min.js') }}"></script> --}}
+    <script src="{{asset('yummy-master/yummy-master/js/bootstrap/bootstrap.min.js')}}"></script>
+    <!-- All Plugins JS -->
+    <script src="{{asset('yummy-master/yummy-master/js/active.js')}}"></script>
+    <!-- Active JS -->
+    <script src="{{asset('yummy-master/yummy-master/js/others/plugins.js')}}"></script>
+
 </body>
