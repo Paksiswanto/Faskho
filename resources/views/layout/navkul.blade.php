f7f29804d436e2f1ab176c4067dcc1d9ec4698d4
paksiswanto <unitedkuryu@gmail.com>
2023-02-28 09:46:43 +0700

    Create Screenshot (97).png

--- resources/views/layout/navkul.blade.php
+++ resources/views/layout/navkul.blade.php
@@ -16,7 +16,9 @@
                 <div class="col-7 col-sm-6">
                     <div class="signup-search-area d-flex align-items-center justify-content-end">
                         <div class="login_register_area  d-flex">
-                     
+                            @auth
+                            <p class="mt-1">Selamat datang {{Auth::user()->name}}</p>
+                            @endauth
                             <div class="user-area dropdown float-right">
                                 <svg class="ml-1"style="width:30"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
