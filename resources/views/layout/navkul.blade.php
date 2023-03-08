6aa191c86ec3a9c0153c9b7004acbfd49cc91ba0
monica152 <126734694+monica152@users.noreply.github.com>
2023-03-08 10:11:32 +0700

    p

--- resources/views/layout/navkul.blade.php
+++ resources/views/layout/navkul.blade.php
@@ -20,8 +20,15 @@
                             <p class="mt-1">Selamat datang {{Auth::user()->name}}</p>
                             @endauth
                             <div class="user-area dropdown float-right">
-                                <svg class="ml-1"style="width:30"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                <svg class="ml-1"style="width:5"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    @auth
+                                        
+                                    @if (Auth::user()->foto)
+                                       <img class="user-avatar rounded-circle" src="{{asset('storage/' . Auth::user()->foto)}}" alt="User Avatar" style="width: 30px">
+                                       @endauth
+                                       @else
+                                        <img class="user-avatar rounded-circle" src="{{ asset('poto.jpg') }}"alt="User Avatar" style="width: 30px">
+                                        @endif
                                   </svg>
                                
                                 </a>
