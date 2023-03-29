456e9d8c51d139f7de37bfce091a0fc459fe1a1d
paksiswanto <unitedkuryu@gmail.com>
2023-03-29 12:44:15 +0700

    as

--- resources/views/dataulasan.blade.php
+++ resources/views/dataulasan.blade.php
@@ -15,69 +15,36 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <strong class="card-title">Data Ulasan</strong>
+                        <strong class="card-title">Pesan</strong>
                     </div>
+                    @foreach ($data as $index=>$row )
                     <div class="card-body">
-                        <table class="table table-striped table-bordered">
-                            <div class="add_button ms-2 mb-3">
-                                <div class="my-3">
-                                    <form action="" method="get">
-                                        <div class="input-group mb-3 col-12 col-sm-8 col-md-6">
-                                                <input type="text" class="form-control" name="keyword" placeholder="Cari Data berdasarkan nama">
-                                                 <button  class="input-group-text btn btn-primary">Search</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Nama</th>
-                                    <th>Email</th>
-                                    <th>laporan</th>
-                                    <th>Tanggal</th>
-
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @php
-                                $no = 1;
-                                @endphp
-                                @foreach ($data as $index=>$row )
-                                <tr>
-                                    <th scope="row">{{ $index +$data->firstitem()}}</th>
-                                    <td>{{ $row->nama }}</td>
-                                    <td>{{ $row->email }}</td>
-                                    <td>{{ $row->laporan }}</td>
-                                    <td>{{ $row->created_at->format('D M Y') }}</td>
-                                    <td>
-                                        <a href="/deletedata/{{ $row->id }}" class="btn btn-danger delete">Hapus</a>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                    <div style="border-bottom:2px solid silver;">
+                    <h5><strong>Anda mendapatkan pesan dari {{ $row->email }}</strong></p>
+                        <p> Berupa {{ $row->laporan }}</p>
+                    </div>
                         <div>
-                            showing
-                            {{ $data->firstitem() }}
-                            to
-                            {{ $data->lastitem() }}
-                            of
-                            {{ $data->total() }}
-                            entries
                         </div>
+                        
                         <div class="pull-right">
                             {{ $data ->withQueryString()-> links() }}
                         </div>
+
                     </div>
+                    @endforeach
                 </div>
-
-
+                showing
+                {{ $data->firstitem() }}
+                to
+                {{ $data->lastitem() }}
+                of
+                {{ $data->total() }}
+                entries
+                
             </div>
         </div><!-- .animated -->
     </div><!-- .content -->
-
+    
     {{-- <div class="content">
     <div class="row">
         <div class="col">
