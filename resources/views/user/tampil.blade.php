31c0841776cef61b65817828445f2a0d9055f8d3
lontongbalap <90811663+Mayfiq@users.noreply.github.com>
2023-05-02 09:48:51 +0700

    t

--- resources/views/user/tampil.blade.php
+++ resources/views/user/tampil.blade.php
@@ -98,13 +98,7 @@
 
                         <!-- Single Post Share Info -->
                         <div class="col-2 col-sm-1">
-                            <div class="single-post-share-info mt-100">
-                                <a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                <a href="#" class="googleplus"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                                <a href="#" class="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                                <a href="#" class="pinterest"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-                            </div>
+                         
                         </div>
 
                         <!-- Single Post -->
